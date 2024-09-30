3e03f2f4cf7b2ad449d31d971c3cab96d44a1894
xurky1 <159796649+xurky1@users.noreply.github.com>
2024-09-30 21:00:01 +0300

    Add files via upload

---
+++ cw2.cpp
@@ -0,0 +1,53 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    int Rational(int numerator, int denominator)
+        : numerator{numerator}
+        , denominator{denominator}
+    {
+        cout << "Constructor\n"
+    }
+
+    ~Rational()
+    {
+        cout << "Deconstructor\n"
+    }
+
+    void print()
+    {
+        cout << numerator << '/' << denominator << '\n';
+    }
+
+    Rational operator*(const Rational& other)
+    {
+        return Rational(
+            this->numerator * other.numerator,
+            this->numerator * other.denominator);
+        )
+
+    Rational operator*(const Rational& other)
+    {
+        return Rational(
+        this->other.deteminator;
+        this->other.numerator;
+        )
+    }
+
+private:
+    int numerator;
+    int denominator;
+};
+
+
+
+int main()
+{
+    Rational rat1(5, 7);
+
+    return 0;
+}
+
